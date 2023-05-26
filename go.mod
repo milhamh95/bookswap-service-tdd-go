4ff66b66a8bee8ed5a96819bacfaef7466a6b866
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2023-05-26 11:06:53 +0700

    test(handler): use http test server to replace fiber

--- go.mod
+++ go.mod
@@ -3,10 +3,21 @@ module bookswap-service-tdd-go
 go 1.20
 
 require (
+	github.com/gofiber/fiber/v2 v2.46.0
+	github.com/google/uuid v1.3.0
+	github.com/onsi/ginkgo/v2 v2.9.5
+	github.com/onsi/gomega v1.27.7
+	github.com/rs/xid v1.5.0
+	github.com/stretchr/testify v1.8.3
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gofiber/fiber/v2 v2.46.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
@@ -14,15 +25,15 @@ require (
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/rs/xid v1.5.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.47.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.9.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -5,131 +5,82 @@ import (
 	"bookswap-service-tdd-go/handlers"
 	"encoding/json"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/rs/xid"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
 
 func TestIndexIntegration(t *testing.T) {
-	if os.Getenv("LONG") == "" {
-		t.Skip("Skipping TestIndexIntegration in short mode")
-	}
+	t.Run("with fiber", func(t *testing.T) {
+		if os.Getenv("LONG") == "" {
+			t.Skip("Skipping TestIndexIntegration in short mode")
+		}
 
-	// Arrange
-	book := db.Book{
-		ID:     xid.New().String(),
-		Name:   "my first integration test",
-		Status: db.Available.String(),
-	}
-	bs := db.NewBookService([]db.Book{book}, nil)
-	ha := handlers.NewHandler(bs, nil)
+		// Arrange
+		book := db.Book{
+			ID:     xid.New().String(),
+			Name:   "my first integration test",
+			Status: db.Available.String(),
+		}
+		bs := db.NewBookService([]db.Book{book}, nil)
+		ha := handlers.NewHandler(bs, nil)
 
-	app := fiber.New()
-	handlers.ConfigureServer(app, ha)
+		app := fiber.New()
+		handlers.ConfigureServer(app, ha)
 
-	req, _ := http.NewRequest(
-		"GET",
-		"/",
-		nil,
-	)
-
-	res, err := app.Test(req, -1)
-	require.Equal(t, http.StatusOK, res.StatusCode)
-	require.NoError(t, err)
-
-	body, err := io.ReadAll(res.Body)
-	res.Body.Close()
-	require.NoError(t, err)
-
-	var resp handlers.Response
-	err = json.Unmarshal(body, &resp)
-	require.NoError(t, err)
-
-	require.Equal(t, 1, len(resp.Books))
-	require.Contains(t, resp.Books, book)
-}
-
-func TestIndexRoute(t *testing.T) {
-	// Define a structure for specifying input and output
-	// data of a single test case. This structure is then used
-	// to create a so called test map, which contains all test
-	// cases, that should be run for testing this function
-	tests := []struct {
-		description string
-
-		// Test input
-		route string
-
-		// Expected output
-		expectedError bool
-		expectedCode  int
-		expectedBody  string
-	}{
-		{
-			description:   "index route",
-			route:         "/",
-			expectedError: false,
-			expectedCode:  200,
-			expectedBody:  "OK",
-		},
-		{
-			description:   "non existing route",
-			route:         "/i-dont-exist",
-			expectedError: false,
-			expectedCode:  404,
-			expectedBody:  "Cannot GET /i-dont-exist",
-		},
-	}
-
-	// Setup the app as it is done in the main function
-	// Initialize a new app
-	app := fiber.New()
-
-	// Register the index route with a simple
-	// "OK" response. It should return status
-	// code 200
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
-	})
-
-	// Iterate through test single test cases
-	for _, test := range tests {
-		// Create a new http request with the route
-		// from the test case
 		req, _ := http.NewRequest(
 			"GET",
-			test.route,
+			"/",
 			nil,
 		)
 
-		// Perform the request plain with the app.
-		// The -1 disables request latency.
 		res, err := app.Test(req, -1)
+		require.Equal(t, http.StatusOK, res.StatusCode)
+		require.NoError(t, err)
 
-		// verify that no error occured, that is not expected
-		assert.Equalf(t, test.expectedError, err != nil, test.description)
-
-		// As expected errors lead to broken responses, the next
-		// test case needs to be processed
-		if test.expectedError {
-			continue
-		}
-
-		// Verify if the status code is as expected
-		assert.Equalf(t, test.expectedCode, res.StatusCode, test.description)
-
-		// Read the response body
 		body, err := io.ReadAll(res.Body)
+		res.Body.Close()
+		require.NoError(t, err)
 
-		// Reading the response body should work everytime, such that
-		// the err variable should be nil
-		assert.Nilf(t, err, test.description)
+		var resp handlers.Response
+		err = json.Unmarshal(body, &resp)
+		require.NoError(t, err)
 
-		// Verify, that the reponse body equals the expected body
-		assert.Equalf(t, test.expectedBody, string(body), test.description)
-	}
+		require.Equal(t, 1, len(resp.Books))
+		require.Contains(t, resp.Books, book)
+	})
+
+	t.Run("with http test new server", func(t *testing.T) {
+		// Arrange
+		book := db.Book{
+			ID:     xid.New().String(),
+			Name:   "my first integration test",
+			Status: db.Available.String(),
+		}
+		bs := db.NewBookService([]db.Book{book}, nil)
+		ha := handlers.NewHandler(bs, nil)
+
+		svr := httptest.NewServer(http.HandlerFunc(adaptor.FiberHandlerFunc(ha.Index)))
+		defer svr.Close()
+
+		r, err := http.Get(svr.URL)
+		require.Equal(t, http.StatusOK, r.StatusCode)
+		require.NoError(t, err)
+
+		body, err := io.ReadAll(r.Body)
+		r.Body.Close()
+		require.NoError(t, err)
+
+		var resp handlers.Response
+		err = json.Unmarshal(body, &resp)
+		require.NoError(t, err)
+
+		require.Equal(t, 1, len(resp.Books))
+		require.Contains(t, resp.Books, book)
+	})
 }
